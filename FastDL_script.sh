724f5ebe3981e94b9c74b79a40dd3cc6f66e50db
Amine <71028463+Amine-Aziz@users.noreply.github.com>
2023-12-13 00:56:47 +0100

    Update FastDL_script.sh

--- FastDL_script.sh
+++ FastDL_script.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-
-server_path="%home_path%"
-fastdl_path="%home_path%/fastdl"
-fastdl_link="http://%ip%:8080/%port%/"
-
-cd $server_folder
+cd %home_path%
 mkdir -p fastdl
-cp -rf $server_path/cstrike/gfx $server_path/cstrike/maps $server_path/cstrike/models $server_path/cstrike/overviews $server_path/cstrike/resource $server_path/cstrike/sound $server_path/cstrike/sprites $server_path/cstrike/*.wad $fastdl_folder
-sed -i '/sv_downloadurl/c sv_downloadurl "$fastdl_link"' $server_path/cstrike/server.cfg
-sed -i '/sv_allowupload/c sv_allowupload 1' $server_path/cstrike/server.cfg
-sed -i '/sv_allowdownload/c sv_allowdownload 1' $server_path/cstrike/server.cfg
-sed -i -e '$a' $server_path/cstrike/server.cfg
-grep -qF -- 'sv_downloadurl "$fastdl_link"' '$server_path/cstrike/server.cfg' || echo 'sv_downloadurl "$fastdl_link"' >> '$server_path/cstrike/server.cfg'
-sed -i -e '$a' $server_path/cstrike/server.cfg
-grep -qF -- 'sv_allowupload 1' '$server_path/cstrike/server.cfg' || echo 'sv_allowupload 1' >> '$server_path/cstrike/server.cfg'
+cp -rf %home_path%/cstrike/gfx %home_path%/cstrike/maps %home_path%/cstrike/models %home_path%/cstrike/overviews %home_path%/cstrike/resource %home_path%/cstrike/sound %home_path%/cstrike/sprites %home_path%/cstrike/*.wad %home_path%/fastdl
+sed -i '/sv_downloadurl/c sv_downloadurl "http://%ip%:8080/%port%/"' %home_path%/cstrike/server.cfg
+sed -i '/sv_allowupload/c sv_allowupload 1' %home_path%/cstrike/server.cfg
+sed -i '/sv_allowdownload/c sv_allowdownload 1' %home_path%/cstrike/server.cfg
 sed -i -e '$a' %home_path%/cstrike/server.cfg
-grep -qF -- 'sv_allowdownload 1' '$server_path/cstrike/server.cfg' || echo 'sv_allowdownload 1' >> '$server_path/cstrike/server.cfg'
-rm -rf $server_path/cstrike/server.cfg.bak
+grep -qF -- 'sv_downloadurl "http://%ip%:8080/%port%/"' '%home_path%/cstrike/server.cfg' || echo 'sv_downloadurl $fastdl_link' >> '%home_path%/cstrike/server.cfg'
+sed -i -e '$a' %home_path%/cstrike/server.cfg
+grep -qF -- 'sv_allowupload 1' '%home_path%/cstrike/server.cfg' || echo 'sv_allowupload 1' >> '%home_path%/cstrike/server.cfg'
+sed -i -e '$a' %home_path%/cstrike/server.cfg
+grep -qF -- 'sv_allowdownload 1' '%home_path%/cstrike/server.cfg' || echo 'sv_allowdownload 1' >> '%home_path%/cstrike/server.cfg'
+rm -rf %home_path%/cstrike/server.cfg.bak
